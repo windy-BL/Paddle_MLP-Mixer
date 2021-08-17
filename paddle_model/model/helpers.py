11190ff2c0fe3bda85b473be768f13bfb3b142bd
windy-BL <79237521+windy-BL@users.noreply.github.com>
2021-08-17 13:28:54 +0800

    Add files via upload

---
+++ paddle_model/model/helpers.py
@@ -0,0 +1,13 @@
+from typing import  Callable
+import paddle.nn as nn
+
+
+def named_apply(fn: Callable, module: nn.Layer, name='', depth_first=True, include_root=False) -> nn.Layer:
+    if not depth_first and include_root:
+        fn(module=module, name=name)
+    for child_name, child_module in module.named_children():
+        child_name = '.'.join((name, child_name)) if name else child_name
+        named_apply(fn=fn, module=child_module, name=child_name, depth_first=depth_first, include_root=True)
+    if depth_first and include_root:
+        fn(module=module, name=name)
+    return module
